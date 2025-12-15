ad8007a55282ebd3561bfecc201b0f2b74a39791
joaquingarciaf9 <joaquingarciaforestello@gmail.com>
2025-12-15 11:57:27 -0300

    update config

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "7350";
+$password = "Alem105800";
 $dbname = "shopping_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
